329d3805609824118a255ff68de24d849c579acd
KevinAlvarezD <kevinadozanam23@gmail.com>
2024-11-06 09:47:09 -0500

    Feat: Manage sessions and token storage on the client

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,7 +69,6 @@
         </style>
     @endif
 </head>
-
 <body class="font-sans antialiased dark:text-white/50">
     <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
         <div
