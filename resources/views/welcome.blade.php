6a797d7f4bfa503129bd07739c5ebaf25082dd26
davison <daviocassio95@gmail.com>
2024-11-06 09:26:44 -0500

    Feat: Send a confirmation email after successful registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -103,7 +103,6 @@
                     </main>
                     
 
-                   
                 </header>
                 <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                     Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
