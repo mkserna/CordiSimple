6bbd404f637d9673806eb0419d99eca5ec13dc18
mkserna <mperezserna8@gmail.com>
2024-11-06 08:06:18 -0500

    feat: Create action of a create new event and EventRequest

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Event;
+use App\Http\Requests\EventRequest;
 
 class EventController extends Controller
 {
-    //
+    // Método para almacenar un nuevo evento en la base de datos.
+    public function store(EventRequest $request)
+    {
+        $validatedData = $request->validated(); // Valida los datos recibidos usando EventRequest.
+        $validatedData['occupied_slots'] = 0; // Inicializa los lugares ocupados en cero.
+        Event::create($validatedData); // Crea el evento en la base de datos usando los datos validados.
+
+        // Redirige a la lista de eventos con un mensaje de éxito.
+        return redirect()->route('events.index')->with('success', 'Evento creado exitosamente.');
+    }
 }
