d4ce9e9b58e548463bbc8aa133995e6d59604aeb
KevinAlvarezD <kevinadozanam23@gmail.com>
2024-11-06 09:46:18 -0500

    Feat: Implement credential validation on the backend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -102,7 +102,6 @@
                         @endif
                     </main>
                     
-
                 </header>
                 <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                     Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
