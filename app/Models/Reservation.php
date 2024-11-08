077538ffc3b362a236d4a8ff6f8d4bb696452372
mkserna <mperezserna8@gmail.com>
2024-11-08 06:39:05 -0500

    feat: Create model and migration event

---
+++ app/Models/Reservation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Reservation extends Model
+{
+    protected $table = "reservations";
+    public $timestamps = true;
+    protected $fillable = [
+        'status',
+        'user_id',
+        'event_id'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class, 'event_id', 'id');
+    }
+}
