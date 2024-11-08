24f4fa85d6b58239a1cd8dfa5bba10c4c70a9477
mkserna <mperezserna8@gmail.com>
2024-11-08 06:50:33 -0500

    feat: Create reservation seeders

--- database/seeders/ReservationSeeder.php
+++ database/seeders/ReservationSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Reservation;
 use Illuminate\Database\Seeder;
 
 class ReservationSeeder extends Seeder
@@ -12,6 +13,23 @@ class ReservationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Crear 3 reservas específicas
+        Reservation::create([
+            'status' => true,
+            'user_id' => 1, // Usuario 1
+            'event_id' => 3, // Evento 1
+        ]);
+
+        Reservation::create([
+            'status' => true,
+            'user_id' => 2, // Usuario 2
+            'event_id' => 4, // Evento 2
+        ]);
+
+        Reservation::create([
+            'status' => true,
+            'user_id' => 4, // Usuario 3
+            'event_id' => 5, // Evento 3
+        ]);
     }
 }
